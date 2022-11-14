4980c3e4845d82ee2bc4b4ce8fa73ec1b47b34f5
Zerexxn <114048736+Zerexxn@users.noreply.github.com>
2022-11-14 22:19:55 +0700

    Update Select Table.sql

--- Select Table.sql
+++ Select Table.sql
@@ -1,1 +1,17 @@
+select * from category;
 
+select * from product;
+
+select * from transaction;
+
+select * from costumer;
+
+select * from approval_status;
+
+select * from transaction where date_transaction = "2022-11-13";
+
+select * from costumer where id = 1;
+
+select * from product where id = 1;
+
+select name, price from product where id = 5;
