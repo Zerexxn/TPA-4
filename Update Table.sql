dc0f9e692003f0af6421ba429c50dcb63abd04ec
Zerexxn <114048736+Zerexxn@users.noreply.github.com>
2022-11-14 22:20:35 +0700

    Update Update Table.sql

--- Update Table.sql
+++ Update Table.sql
@@ -1,1 +1,7 @@
+update costumer set fullname = "Erick jozz" where id = 3;
 
+update product set price = 12500 where id = 2;
+
+update category set name = "susu anak" where id = 11;
+
+update transaction set date_transaction = "2022-09-20" where id = 3;
